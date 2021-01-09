b4d617e3a909e42a38494d9fc5c244eb708c9fbe
nicolasarcay <nico.arcay@gmail.com>
2021-01-09 09:38:56 -0300

    agrego JS y modificaciones menores en el formulario

---
+++ js/my-js.js
@@ -0,0 +1,21 @@
+class Cotizacion {
+
+	company = '';
+	desing = '';
+	site = '';
+
+	constructor (company, desing, site) {
+		this.company = company;
+		this.desing = desing;
+		this.site = site;
+	}
+	verCotizacion(){
+
+	}
+	enviarCotizacion(){
+
+	}
+	
+}
+
+let cotizacion1 = new Cotizacion(grande, bootstrap, corporativo);
