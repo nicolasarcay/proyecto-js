84eaa289acf0eca346b2d50ca5e414713f74545b
Nicolas Arcay <hola@nicolasarcay.com>
2021-01-22 17:29:34 -0300

    funcionalidad js para mostrar informacion en la sidebar

--- js/my-js.js
+++ js/my-js.js
@@ -1,21 +1,107 @@
-class Cotizacion {
-
-	company = '';
-	desing = '';
-	site = '';
-
-	constructor (company, desing, site) {
-		this.company = company;
-		this.desing = desing;
-		this.site = site;
-	}
-	verCotizacion(){
-
-	}
-	enviarCotizacion(){
-
-	}
 	
-}
+	function buildNombre(nombre){
+		const htmlNombre = `
+		<h3>Hola, ${nombre}</h3> 
+		<h4>Gracias por comenzar con la cotización!</h4>
+		`;
+		return htmlNombre;
+	};
 
-let cotizacion1 = new Cotizacion(grande, bootstrap, corporativo);
+	function buildEmpresa(empresa){
+		const htmlEmpresa = `
+		<h5>El Presupuesto es para <span class="h4">${empresa}</span></h5>
+		<h5>Perfecto! Entonces Continuemos...</h5>
+		`;
+		return htmlEmpresa;
+	};
+	function buildDesign(design){
+		const htmlDesign = `
+		<h5>El diseño lo haremos en <span class="h4">${design}</span></h5>
+		<h5>Perfecto! Entonces Continuemos...</h5>
+		`;
+		return htmlDesign;
+	};
+	const nombre = document.getElementById('nombre')
+	const empresa = document.getElementById('empresa')
+	const design = document.getElementById('design')
+	
+	function onChange(event){
+		nombre.innerHTML = buildNombre(document.querySelector('#name').value);
+	};
+	function onEmpresa(event){
+		empresa.innerHTML = buildEmpresa(document.querySelector('#company').value);
+	};
+	
+	function onDesign(event){
+		if (document.getElementById('wordpress').checked === true){
+			design.innerHTML = buildDesign('Wordpress');
+
+		}else if (document.getElementById('bootstrap').checked === true) {
+			design.innerHTML = buildDesign('Bootstrap');
+		}
+	};
+
+	/*class Cotizacion {
+
+	tipo = '';
+	forma = '';
+	cliente = '';
+
+	constructor (tipo, forma, cliente) {
+		this.tipo = tipo;
+		this.forma = forma;
+		this.cliente = cliente;
+	};
+	function cotizar(){
+		
+		let resultado = 0;
+
+		let precioBase = 0;
+		switch(this.tipo){
+			case "one-page":
+				precioBase = 15000;
+				break;
+			case "corporativo":
+				precioBase = 20000;
+				break;
+			case "tienda":
+				precioBase = 30000;
+				break;
+		}
+		switch(this.forma){
+			case "wordpress":
+				resultado = precioBase * 1.2
+				break;
+			case "bootstrap":
+				resultado = precioBase * 1.3
+				break;
+		}
+		switch(this.cliente){
+			case "grande":
+				cotizacion = resultado * 1.4
+				break;
+			case "mediana":
+				cotizacion = resultado * 1.3
+				break;
+			case "chica":
+				cotizacion = resultado * 1.2
+				break;
+			case "sin-lucro":
+				cotizacion = resultado * 1.1
+				break;
+		}
+		return cotizacion;
+	}
+	};*/
+
+	function onVerCotizacion(){
+
+		let tipo = document.getElementById('tipo').value;
+		let forma = document.getElementById('forma').value; 
+		let cliente = document.getElementById('cliente').value; 
+		console.log(tipo, forma, cliente)
+		/*let cotizacion1 = new Cotizacion(tipo, forma, cliente);
+		let total = cotizacion.cotizar();
+
+		document.getElementbyId('total').value = total;*/
+	};
